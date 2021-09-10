1e31727412abc0190b8b5de0707870753da7d6de
stanta <st.takt@gmail.com>
2021-09-10 13:03:27 +0300

    added opensea-js

---
+++ packages/opensea-js/src/index.ts
@@ -0,0 +1,38 @@
+/* eslint-disable header/header */
+// [object Object]
+// SPDX-License-Identifier: Apache-2.0
+
+import { OpenSeaAPI } from './api';
+import { OpenSeaPort } from './seaport';
+import { EventData, EventType, Network, OpenSeaAPIConfig } from './types';
+
+export { orderToJSON, orderFromJSON, WyvernProtocol } from './utils/utils';
+export {
+  encodeCall,
+  encodeSell, encodeAtomicizedBuy, encodeAtomicizedSell,
+  encodeDefaultCall, encodeReplacementPattern,
+  AbiType
+} from './utils/schema';
+
+/**
+ * Example setup:
+ *
+ * import * as Web3 from 'web3'
+ * import { OpenSeaPort, Network } from 'opensea-js'
+ * const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io')
+ * const client = new OpenSeaPort(provider, {
+ *   networkName: Network.Main
+ * })
+ */
+
+export {
+  // Main SDK export:
+  OpenSeaPort,
+  // So the API could be used separately:
+  OpenSeaAPI,
+  // Useful for serializing and deserializing orders:
+  // Types to help initialize SDK and listen to events.
+  // Can also be imported using e.g.
+  //   import { EventType } from 'opensea-js/lib/types'
+  EventData, EventType, Network, OpenSeaAPIConfig
+};
